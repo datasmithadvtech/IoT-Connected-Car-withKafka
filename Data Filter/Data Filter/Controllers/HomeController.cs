71d2d60bbfc1750a4a285f400fd216b822a6c76c
Ramadan Nasr <ramadannasr40@gmail.com>
2018-08-23 22:32:52 +0200

    Data Filter Service (Asp.net web API)
    Data Filter Service (Asp.net web API)

---
+++ Data Filter/Data Filter/Controllers/HomeController.cs
@@ -0,0 +1,86 @@
+﻿
+using Microsoft.CSharp.RuntimeBinder;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Dynamic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Data_Filter.Models;
+using Newtonsoft.Json.Linq;
+namespace Data_Filter.Controllers
+{
+    public class HomeController : ApiController
+    {
+        public static List<JObject> list = new List<JObject>();
+        // GET: api/Home
+        public IEnumerable<JObject>  Get()
+        {
+            return list;
+        }
+
+        // GET: api/Home/1
+		// IEnumerable<JObject> list is act as queue 
+		// id = 1 will return the First Json Object from the list and delete it 
+        public HttpResponseMessage Get(int id)
+        {
+            var field = list.FirstOrDefault();
+            if (field == null)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "There is No Data Yet.");
+            }
+            else {
+                list.Remove(field);
+                return Request.CreateResponse(HttpStatusCode.OK, field);
+                 }
+        }
+
+        // POST: api/Home
+		// recieve the Posted json String and make Filteration 
+        public HttpResponseMessage Post([FromBody]dynamic postedJson)
+        {
+            HttpResponseMessage msg = null;
+          
+            string fuelLevelInput = postedJson.fuel_level_input != null ? postedJson.fuel_level_input : null;
+            string massAirFlow = postedJson.maf_airflow != null ? postedJson.maf_airflow : null;
+            string rpm = postedJson.rpm != null ? postedJson.rpm : null;
+            string intakeManifoldPressure = postedJson.intake_manifold_pressure != null ? postedJson.intake_manifold_pressure : null;
+            string intakeAirTemp = postedJson.intake_air_temp != null ? postedJson.intake_air_temp : null;
+            string vehicleSpeed = postedJson.vehicle_speed != null ? postedJson.vehicle_speed : null;
+            
+           
+            if ((fuelLevelInput == null || fuelLevelInput == "") || (vehicleSpeed == null || vehicleSpeed == "")) {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Rejecting $payload because either fuelLevelInput or vehicleSpeed are empty");
+            }
+            else
+            {
+                if ((massAirFlow == null || massAirFlow == "")) {
+                    if ((rpm !=null || rpm == "") || (intakeAirTemp != null || intakeAirTemp == "") || (intakeManifoldPressure != null || intakeManifoldPressure == "")) {
+                        return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Rejecting $payload because maf related fields are not available");      
+                 }
+                }
+            }
+            if(addToList(postedJson))
+            msg = Request.CreateResponse(HttpStatusCode.Created, "Added"+list.Count());
+              //  Debug.WriteLine(value);
+                return msg;    
+        }
+
+		// add the filtered Json to the List
+        private bool addToList(dynamic item)
+        {
+            JObject postedJson = (JObject)item;
+          
+            try { 
+            list.Add(postedJson);
+                return true;
+            }catch(Exception ex)
+            {
+                Debug.WriteLine("Exception : "+ex.Message );
+                return false;
+            }
+        }
+    }
+}
---
+++ Data Filter/Data Filter/Models/Field.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Data_Filter.Models
+{
+    public class Field
+    {
+        public double bearing { get; set; }
+        public double catalyst_temp { get; set; }
+        public string vin { get; set; }
+        public double absolute_throttle_pos_b { get; set; }
+        public int fuel_level_input { get; set; }
+        public string intake_manifold_pressure { get; set; }
+        public double maf_airflow { get; set; }
+        public int barometric_pressure { get; set; }
+        public double accelerator_throttle_pos_d { get; set; }
+        public double accelerator_throttle_pos_e { get; set; }
+        public string journeyId { get; set; }
+        public double rpm { get; set; }
+        public double acceleration { get; set; }
+        public double relative_throttle_pos { get; set; }
+        public long time_since_engine_start { get; set; }
+        public int distance_with_mil_on { get; set; }
+        // public long timestamp { get; set; }
+        public int[] fuel_system_status { get; set; }
+        // public double mpg_instantaneous { get; set; }
+        public double control_module_voltage { get; set; }
+        public int intake_air_temp { get; set; }
+        public double latitude { get; set; }
+        public double long_term_fuel { get; set; }
+        // public string available_pids_0_20 { get; set; }
+        // public string available_pids_41_60 { get; set; }
+        public double short_term_fuel { get; set; }
+        public double engine_load { get; set; }
+        public int obd_standards { get; set; }
+        public double longitude { get; set; }
+        public int throttle_position { get; set; }
+        public double vehicle_speed { get; set; }
+        // public string available_pids_21_40 { get; set; }
+        public int coolant_temp { get; set; }
+    }
+}
